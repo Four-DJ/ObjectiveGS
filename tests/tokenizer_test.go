ac503c7bcbae01c948478b41c969fd66855b2751
four-dj <janweidenhammer44@gmail.com>
2025-12-24 21:28:47 +0100

    added multiple inputs for tests

--- tests/tokenizer_test.go
+++ tests/tokenizer_test.go
@@ -9,13 +9,13 @@ import (
 func TestTokenize(t *testing.T) {
 	tests := []struct {
 		Case        string
-		Input       string
+		Input       []string
 		Expected    []transpiler.Token
 		shouldError bool
 	}{
 		{
 			Case:  "valid Tokenization of semicolon",
-			Input: ";",
+			Input: []string{";"},
 			Expected: []transpiler.Token{
 				{
 					Type: transpiler.EOF,
@@ -25,7 +25,7 @@ func TestTokenize(t *testing.T) {
 		},
 		{
 			Case:  "valid Tokenization of namespace",
-			Input: "namespace Test;",
+			Input: []string{"namespace Test;"},
 			Expected: []transpiler.Token{
 				{
 					Type: transpiler.Namespace,
@@ -42,7 +42,7 @@ func TestTokenize(t *testing.T) {
 		},
 		{
 			Case:  "valid Tokenization of clases",
-			Input: "class Test {",
+			Input: []string{"class Test {", "class Test{"},
 			Expected: []transpiler.Token{
 				{
 					Type: transpiler.Class,
@@ -60,12 +60,14 @@ func TestTokenize(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.Case, func(t *testing.T) {
-			actual, err := transpiler.Tokenize(test.Input)
-			if err != nil && !test.shouldError {
-				t.Errorf("%s failed because of triggered error: \n%s", test.Case, err)
-			}
-			if !reflect.DeepEqual(actual, test.Expected) {
-				t.Errorf("%s failed because \nexpected:\n%v\nactual:\n%v", test.Case, test.Expected, actual)
+			for _, input := range test.Input {
+				actual, err := transpiler.Tokenize(input)
+				if err != nil && !test.shouldError {
+					t.Errorf("%s failed because of triggered error: \n%s", test.Case, err)
+				}
+				if !reflect.DeepEqual(actual, test.Expected) {
+					t.Errorf("%s failed because \nexpected:\n%v\nactual:\n%v", test.Case, test.Expected, actual)
+				}
 			}
 		})
 	}
